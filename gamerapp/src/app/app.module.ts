e2cc2d29f9989b09cd7c0612927bca87f407a1f7
Killerbby <martinskimberly2@gmail.com>
2021-12-01 21:44:49 -0300

    up

--- gamerapp/src/app/app.module.ts
+++ gamerapp/src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './views/header/header.component';
+import { HomeComponent } from './views/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
